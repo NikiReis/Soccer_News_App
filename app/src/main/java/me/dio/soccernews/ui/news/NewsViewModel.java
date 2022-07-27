6b8833ad2a51d9df2d4d4fa0f80c55dfa13a9d08
NikiReis <linekreis@hotmail.com>
2022-07-27 15:06:20 -0700

    Including the Retrofit and the Picasso

--- app/src/main/java/me/dio/soccernews/ui/news/NewsViewModel.java
+++ app/src/main/java/me/dio/soccernews/ui/news/NewsViewModel.java
@@ -4,25 +4,48 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import me.dio.soccernews.data.remote.SoccerNewsApi;
 import me.dio.soccernews.domain.News;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class NewsViewModel extends ViewModel {
 
-    private final MutableLiveData<List<News>> news;
+    private final MutableLiveData<List<News>> news = new MutableLiveData<>();
+    private final SoccerNewsApi api;
 
     public NewsViewModel() {
-        this.news = new MutableLiveData<>();
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://nikireis.github.io/Soccer_News_API/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
 
-        // TODO: REMOVER MOCK DE NOTÍCIAS
-        List<News> news = new ArrayList<>();
-        news.add(new News("Ferroviária Tem...","Brasileirao"));
-        news.add(new News("Man City joga...","Premier League"));
-        news.add(new News("Copa do Mundo 2022..","World Cup"));
+        api = retrofit.create(SoccerNewsApi.class);
+        this.findNews();
+    }
 
-        this.news.setValue(news);
+    private void findNews() {
+        api.getNews().enqueue(new Callback<List<News>>() {
+            @Override
+            public void onResponse(Call<List<News>> call, Response<List<News>> response) {
+                if (response.isSuccessful()){
+                    news.setValue(response.body());
+                }else{
+                    // TODO: Pensar em uma estratégia de tratamento de erros
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<News>> call, Throwable t) {
+                // TODO: Pensar em uma estratégia de tratamento de erros
+
+            }
+        });
     }
 
     public LiveData<List<News>> getNews() {
