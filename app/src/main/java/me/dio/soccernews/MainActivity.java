17114fa2a9825cf9f656e83285e2e90433dc6aad
NikiReis <linekreis@hotmail.com>
2022-08-01 07:41:30 -0700

    Including the 'Room' and the 'share' and 'favorite' functionality

--- app/src/main/java/me/dio/soccernews/MainActivity.java
+++ app/src/main/java/me/dio/soccernews/MainActivity.java
@@ -9,12 +9,21 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import androidx.room.Room;
 
+import me.dio.soccernews.data.local.AppDatabase;
 import me.dio.soccernews.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+
+    public AppDatabase getDb() {
+        return db;
+    }
+
+    private AppDatabase db;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +40,15 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+        this.setupDB();
+
+    }
+
+    private void setupDB() {
+        db = Room.databaseBuilder(this, AppDatabase.class,"soccer-news")
+                .allowMainThreadQueries()
+                .build();
     }
 
 }
--- app/src/main/java/me/dio/soccernews/ui/adapter/NewsAdapter.java
+++ app/src/main/java/me/dio/soccernews/ui/adapter/NewsAdapter.java
@@ -1,5 +1,6 @@
 package me.dio.soccernews.ui.adapter;
 
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.view.LayoutInflater;
@@ -12,15 +13,18 @@ import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
+import me.dio.soccernews.R;
 import me.dio.soccernews.databinding.NewsItemBinding;
 import me.dio.soccernews.domain.News;
 
 public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder>{
 
-    private List<News> news;
+    private final List<News> news;
+    private FavoriteListener favoriteListener;
 
-    public NewsAdapter(List<News> news){
+    public NewsAdapter(List<News> news, FavoriteListener favoriteListener){
         this.news = news;
+        this.favoriteListener = favoriteListener;
     }
 
     @NonNull
@@ -33,15 +37,38 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder>{
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Context context = holder.itemView.getContext();
+
         News news = this.news.get(position);
-        holder.binding.tvTitle.setText(news.getTitle());
-        holder.binding.tvText.setText(news.getDescription());
-        Picasso.get().load(news.getImage()).fit().into(holder.binding.imageView);
+        holder.binding.tvTitle.setText(news.title);
+        holder.binding.tvText.setText(news.description);
+        Picasso.get().load(news.image).fit().into(holder.binding.imageView);
+
+        // implementação da funcionalidade de abrir link
         holder.binding.btOpenLink.setOnClickListener(view -> {
             Intent i = new Intent(Intent.ACTION_VIEW);
-            i.setData(Uri.parse(news.getLink()));
-            holder.itemView.getContext().startActivity(i);
+            i.setData(Uri.parse(news.link));
+            context.startActivity(i);
         });
+        // implementação da funcionalidade de compartilhar
+        holder.binding.imageView5.setOnClickListener(view -> {
+            Intent i = new Intent(Intent.ACTION_SEND);
+            i.setType("text/plain");
+            i.putExtra(Intent.EXTRA_TEXT, news.link);
+            context.startActivity(Intent.createChooser(i,"Compartilhar via"));
+        });
+
+        // implementação da funcionalidade de favoritar
+        holder.binding.imageView4.setOnClickListener(view -> {
+            news.favorite = !news.favorite;
+            this.favoriteListener.onFavorite(news);
+            notifyItemChanged(position);
+        });
+        if(news.favorite){
+            holder.binding.imageView4.setColorFilter(context.getResources().getColor(R.color.apply_fav_default));
+        }else{
+            holder.binding.imageView4.setColorFilter(context.getResources().getColor(R.color.fav_default_base));
+        }
     }
 
 
@@ -59,5 +86,9 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder>{
                 this.binding = binding;
             }
         }
+    public interface FavoriteListener {
+        void onFavorite(News news);
     }
+
+}
 
